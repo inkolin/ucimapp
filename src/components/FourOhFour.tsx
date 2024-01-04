725c84274a8cade7cdcf584066c8aa3be396ef2e
Nenad Nikolin <nenad@nikolin.eu>
2024-01-04 15:25:24 +0100

    ubacio sam u vite config env API kljuceve

---
+++ src/components/FourOhFour.tsx
@@ -0,0 +1,6 @@
+function FourOhFour() {
+
+    return <h3>Sajt nije pronadjen 404!</h3>
+}
+
+export default FourOhFour;
---
+++ src/components/Message.tsx
@@ -0,0 +1,6 @@
+function Message() {
+    return <h2>Ovo je Message komponenta</h2>;
+
+}
+
+export default Message;
